e62cc0a2822ed5b9a8b6ff83d5040296e2cc55d6
Axel <axosar@gmail.com>
2021-08-17 12:21:39 -0500

    functions typed

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -6,3 +6,34 @@ const substract = (a:number, b:number): number => {
   return a - b;
 };
 
+function divide(a:number, b:number): number {
+  return a / b;
+};
+
+const multiply = function(a: number, b: number): number {
+  return a * b;
+};
+
+// void no return from this function
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  if(!message){
+    throw new Error(message);
+  }
+  
+  return message;
+};
+
+const forecast = {
+  date: new Date(),
+  weather: 'sunny'
+};
+
+const logWeather = ({ date, weather }: { date: Date, weather: string}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
