b9ead1d5cac391a16a62dbba797a1e1b57c4f16c
Axel <axosar@gmail.com>
2021-08-19 09:46:02 -0500

    functions in interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,19 +1,21 @@
 interface Vehicle {
   name: string; 
   year: Date; 
-  broken: boolean
+  broken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
   year: new Date(),
-  broken: true
+  broken: true,
+  summary(): string {
+    return `Name ${this.name}`;
+  }
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken: ${vehicle.broken}`);
+  console.log(vehicle.summary);
 };
 
 printVehicle(oldCivic);
