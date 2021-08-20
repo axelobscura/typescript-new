76b32f914478d342a496b1a3072e7fd3f4d15c35
Axel <axosar@gmail.com>
2021-08-20 09:52:23 -0500

    basiv inheritance

--- features/classes.ts
+++ features/classes.ts
@@ -2,7 +2,17 @@ class Vehicle {
   drive(): void {
     console.log('chugga chugga');
   }
+  honk(): void {
+    console.log('honkydook');
+  }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
+class Car extends Vehicle {
+  drive(): void {
+    console.log('vrrroooooommmmmm....')
+  }
+}
+
+const car = new Car();
+car.drive();
+car.honk();
