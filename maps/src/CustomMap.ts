1c3b043e802f37386657e60cf1b692a248e9e7f5
Axel <axosar@gmail.com>
2021-08-30 13:07:34 -0500

    showing popup windows

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -20,13 +20,21 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void{
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
-    })
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi!'
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+
   }
 
 }
